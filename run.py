5a9ef9ca84422f456a438ba6512aa477c5df19cf
chava28pvp <129213697+chava28pvp@users.noreply.github.com>
2025-06-11 16:51:01 -0600

    fix: action of open a new web page every time the page was reloaded was fixed

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 from app import create_app
 import webbrowser
 import threading
+import os
 
 app = create_app()
 
@@ -9,5 +10,6 @@ def open_browser():
     webbrowser.open_new("http://localhost:5000/apidocs")
 
 if __name__ == '__main__':
-    threading.Timer(1.0, open_browser).start()
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "false":
+        threading.Timer(1.0, open_browser).start()
     app.run(debug=True)
